96837359e671de307a93b5adf75cdc7573eb38f1
meets45 <mits5455@gmail.com>
2022-11-16 15:29:27 +0530

    Navbar Fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,77 +1,75 @@
-import React from 'react'
-import {actionCreators} from '../state'
-import {useSelector, useDispatch} from 'react-redux'
+import React from "react";
+import { actionCreators } from "../state";
+import { useSelector, useDispatch } from "react-redux";
 
 function Navbar() {
   // Navbar component to display a navbar
 
-  const dispatch = useDispatch()
-  const mode = useSelector((state) => state.theme)
+  const dispatch = useDispatch();
+  const mode = useSelector((state) => state.theme);
   const toggleMode = () => {
-    if (mode === 'light') {
-      dispatch(actionCreators.darkTheme())
-      document.body.style.backgroundColor = 'white'
+    if (mode === "light") {
+      dispatch(actionCreators.darkTheme());
+      document.body.style.backgroundColor = "white";
     } else {
-      dispatch(actionCreators.lightTheme())
-      document.body.style.backgroundColor = '#121212'
+      dispatch(actionCreators.lightTheme());
+      document.body.style.backgroundColor = "#121212";
     }
-  }
+  };
   return (
     <nav
       className={`navbar navbar-${
-        mode === 'light' ? 'dark' : 'light'
+        mode === "light" ? "dark" : "light"
       } navbar-expand-lg bg-${
-        mode === 'light' ? 'dark' : 'light'
+        mode === "light" ? "dark" : "light"
       } navbar-fixed`}
     >
-      <div className='container-fluid'>
-        <a className='navbar-brand fs-4' href='/'>
-          Todolist
-        </a>
+      <div className="container-fluid">
+        <span className="navbar-brand fs-4">Todolist</span>
         <button
-          className='navbar-toggler'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarSupportedContent'
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          <span className='navbar-toggler-icon'></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-          <ul className='navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-end'></ul>
-          <div
-            onClick={toggleMode}
-            className='cursor hover'
-            title='Change Theme'
-          >
-            <label
-              htmlFor='theme'
-              className={`text-${
-                mode === 'light' ? 'light' : 'dark'
-              } me-1 cursor margin`}
-            >
-              {`${mode === 'light' ? 'Light ' : 'Dark '}`} Mode
-            </label>
-            <i
-              id='theme'
-              className={`fa-solid fa-${
-                mode === 'light' ? 'sun' : 'moon'
-              } text-${
-                mode === 'light' ? 'light fs-5 me-3' : 'dark fs-5 me-2'
-              }`}
-            ></i>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-end"></ul>
+          <div onClick={toggleMode} title="Change Theme">
+            <span className="hover">
+              <label
+                htmlFor="theme"
+                className={`text-${
+                  mode === "light" ? "light" : "dark"
+                } me-1 margin cursor`}
+              >
+                {`${mode === "light" ? "Light " : "Dark "}`} Mode
+              </label>
+              <i
+                id="theme"
+                className={`fa-solid cursor fa-${
+                  mode === "light" ? "sun" : "moon"
+                } text-${
+                  mode === "light" ? "light fs-6 me-3" : "dark fs-6 me-2"
+                }`}
+              ></i>
+            </span>
           </div>
           <span
-            className={`text-${mode === 'light' ? 'light' : 'dark'} fs-6 me-2`}
+            className={`text-${
+              mode === "light" ? "light" : "dark"
+            } fs-6 me-2 mt-1`}
           >
             Stay organized & focused.
           </span>
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
